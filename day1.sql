ecf8a67e23074283983bf76b5c3f7e24a11607af
Gani25 <memonabdulgani3d@gmail.com>
2025-08-02 16:02:56 +0530

    Update day1.sql

--- day1.sql
+++ day1.sql
@@ -77,4 +77,48 @@ end $
 
 delimiter ;
 
-call find_cust_info(10,2);
+call find_cust_info(1,100);
+
+delimiter $
+drop procedure if exists find_cust_info_sort $
+create procedure find_cust_info_sort
+(
+	cust_id int,
+    page_size int,
+    sort_column text,
+    sort_direction text
+)
+begin
+
+	set @sql_query = concat('
+		select cust.customer_id, cust.name, status, 
+		date_format(order_date,"%d-%m-%y") order_date,
+		prod.name, category, round(price * quantity,2) total
+		from customers cust
+		join orders ods on cust.customer_id = ods.customer_id
+		join order_items o_its on ods.order_id = o_its.order_id
+		join products prod on o_its.product_id = prod.product_id
+		where cust.customer_id = ',cust_id,
+        ' order by ',sort_column, 
+        if(upper(sort_direction) = "DESC", " DESC"," ASC")
+        ,
+		' limit ', page_size
+        );
+        
+        
+        prepare stmnt from @sql_query;
+        execute stmnt;
+        deallocate prepare stmnt;
+    
+	
+    
+end $
+
+delimiter ;
+/*
+Error Code: 1064. You have an error in your SQL syntax; 
+check the manual that corresponds to your MySQL server version 
+for the right syntax to use near '101' at line 1
+
+*/
+call find_cust_info_sort(1,100,"total","Desc");
